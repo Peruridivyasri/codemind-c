f71eb62af74241349d776e6a0128887a53c08436
Peruridivyasri <120235440+Peruridivyasri@users.noreply.github.com>
2023-01-11 11:13:21 +0530

    #include<stdio.h> int main() {     float a,b,c,d;     scanf("%f",&a);     if(a<=199)     {         b=(a*1.20)+100;         printf("%0.2f",b);     }     else if(a>=200&&a<400)     {         b=(a*1.50)+100;         printf("%0.2f",b);     }     else if(a>=400&&a<600)     {         b=a*1.80;         c=b*0.15;         d=b+c;         printf("%0.2f",d);     }     else if(a>600)     {         b=a*2.00;         c=b*0.15;         d=b+c;         printf("%0.2f",d);     } }

---
+++ Current_bill.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d;
+    scanf("%f",&a);
+    if(a<=199)
+    {
+        b=(a*1.20)+100;
+        printf("%0.2f",b);
+    }
+    else if(a>=200&&a<400)
+    {
+        b=(a*1.50)+100;
+        printf("%0.2f",b);
+    }
+    else if(a>=400&&a<600)
+    {
+        b=a*1.80;
+        c=b*0.15;
+        d=b+c;
+        printf("%0.2f",d);
+    }
+    else if(a>600)
+    {
+        b=a*2.00;
+        c=b*0.15;
+        d=b+c;
+        printf("%0.2f",d);
+    }
+}
